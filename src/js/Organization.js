b4a76c8146062ba367083af2adb6e0bfdc4c4f8b
Justin Burger <j@justinburger.com>
2012-11-18 17:53:42 -0800

    Working on wizard validation.

--- src/js/Organization.js
+++ src/js/Organization.js
@@ -63,3 +63,54 @@ function LoadStep1OrgList(){
     wizard_step(0,1);
 
 }
+
+function wizard_step(from, to){
+    
+
+
+    if(to ==2){
+        org = $('#step1_organization_list').children('.btn-group button.active').html();
+
+        if(org == '' || org == undefined){
+            $('#step1_organization_list').tooltip('show')
+            return;
+        }
+    }else if(to ==5){
+        environment = $('#step4_organization_list').children('.btn-group button.active').html();
+
+        if(environment == '' || environment == undefined){
+            $('#step4_organization_list').tooltip('show')
+            return;
+        }
+    }
+    jQuery('#myCarousel').carousel('next');
+    jQuery('#myCarousel').carousel('pause');
+
+
+}
+
+function toggleConsole(){
+    if(jQuery('#console_btn').html() == "Hide Console"){
+        jQuery('#tool_pane').removeClass('span4');
+        jQuery('#tool_pane').addClass('span11');
+
+        jQuery('#console_pane').hide();
+
+        jQuery('#console_btn').removeClass('btn-danger');
+        jQuery('#console_btn').addClass('btn-inverse');
+
+        jQuery('#console_btn').html('Show Console');
+    }else{
+        jQuery('#tool_pane').removeClass('span11');
+        jQuery('#tool_pane').addClass('span4');
+
+        jQuery('#console_pane').show();
+        jQuery('#console_pane').addClass('span8');
+
+        jQuery('#console_btn').removeClass('btn-inverse');
+        jQuery('#console_btn').addClass('btn-danger');
+
+        jQuery('#console_btn').html('Hide Console');
+    }
+
+}
