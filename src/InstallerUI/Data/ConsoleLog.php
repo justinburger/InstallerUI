2deb6c8c6930805d800a4368e7f9831265ee7c59
Justin Burger <j@justinburger.com>
2012-11-21 18:54:40 -0800

    Creating Data object to interact with Console Memcache logging.
    Deployment tool logs to memcache, this will pick it up.

---
+++ src/InstallerUI/Data/ConsoleLog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jburger
+ * Date: 11/21/12
+ * Time: 6:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ConsoleLog{
+    public function getConsoleRows(){}
+    
+}
